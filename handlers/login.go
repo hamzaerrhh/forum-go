bcdcfe7cad45e38ccb07541c9965ba2747d64071
halhyane <houssamalhyan5@gmail.com>
2026-02-19 12:55:22 +0000

    update login

--- handlers/login.go
+++ handlers/login.go
@@ -4,49 +4,66 @@ import (
 	"database/sql"
 	"html/template"
 	"net/http"
+
 	"zone/database"
+
+	"github.com/google/uuid"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/Login" {
-		http.NotFound(w, r)
+		HandleError(w, http.StatusNotFound, "Page not found")
 		return
 	}
 
 	if r.Method == http.MethodPost {
-
 		email := r.FormValue("email")
 		password := r.FormValue("password")
 
-		var dbPassword string
-
+		var dbPassword, userName string
 		err := database.Database.QueryRow(
-			"SELECT password FROM users WHERE email = ?",
+			"SELECT password, userName FROM users WHERE email = ?",
 			email,
-		).Scan(&dbPassword)
-
+		).Scan(&dbPassword, &userName)
 		if err == sql.ErrNoRows {
-			http.Error(w, "User not found", 401)
+			HandleError(w, 401, "User not found")
 			return
 		}
-
 		if err != nil {
-			http.Error(w, "Server error", 500)
+			HandleError(w, 500, "User not found")
 			return
 		}
 
 		if password != dbPassword {
-			http.Error(w, "Wrong password", 401)
+			HandleError(w, 401, "Wrong password")
+
 			return
 		}
 
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		sessionID := uuid.NewString()
+		_, err = database.Database.Exec(
+			"UPDATE users SET session = ?, dateexpired = DATETIME('now', '+24 hours') WHERE email = ?",
+			sessionID, email,
+		)
+		if err != nil {
+			HandleError(w, 500, "Server error")
+			return
+		}
+
+		http.SetCookie(w, &http.Cookie{
+			Name:     "session_id",
+			Value:    sessionID,
+			Path:     "/",
+			HttpOnly: true,
+		})
+
+		http.Redirect(w, r, "/Dashboard", http.StatusSeeOther)
 		return
 	}
 
 	tmpl, err := template.ParseFiles("templates/Login.html")
 	if err != nil {
-		http.Error(w, "Template error", 500)
+		HandleError(w, 500, "Template error")
 		return
 	}
 
