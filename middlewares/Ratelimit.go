178bfe4d817f021403df875ffcc2191208e4d42a
halhyane <houssamalhyan5@gmail.com>
2026-02-28 22:21:51 +0000

    update count to time

--- middlewares/Ratelimit.go
+++ middlewares/Ratelimit.go
@@ -10,58 +10,44 @@ import (
 	"forum/handlers"
 )
 
-func RateLimit(handler http.HandlerFunc, maxRequests int, window time.Duration) http.HandlerFunc {
+func RateLimit(handler http.HandlerFunc, minInterval time.Duration) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			handler(w, r)
 			return
 		}
+
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			handlers.HandleError(w, http.StatusInternalServerError, "Invalid IP address")
 			return
 		}
 
-		var count int // number of requests
 		var lastRequest time.Time
 
 		err = database.Database.QueryRow(
-			"SELECT count, last_request FROM rate_limits WHERE ip = ?", ip,
-		).Scan(&count, &lastRequest)
+			"SELECT last_request FROM rate_limits WHERE ip = ?", ip,
+		).Scan(&lastRequest)
 
 		if err == sql.ErrNoRows {
-			_, err = database.Database.Exec(
-				"INSERT INTO rate_limits (ip, count, last_request) VALUES (?, ?, ?)",
-				ip, 1, time.Now(),
+			database.Database.Exec(
+				"INSERT INTO rate_limits (ip, last_request) VALUES (?, ?)", ip, time.Now(),
 			)
-			if err != nil {
-				handlers.HandleError(w, http.StatusInternalServerError, "Internal Server Erro")
-				return
-			}
 			handler(w, r)
 			return
 		} else if err != nil {
-			handlers.HandleError(w, http.StatusInternalServerError, "Internal Server Erro")
+			handlers.HandleError(w, http.StatusInternalServerError, "Internal Server Error")
 			return
 		}
 
-		if time.Since(lastRequest) > window {
-			count = 0
-		}
-
-		if count >= maxRequests {
-			handlers.HandleError(w, http.StatusTooManyRequests, "Rate limit exceeded. Please try again later.")
+		if time.Since(lastRequest) < minInterval {
+			handlers.HandleError(w, http.StatusTooManyRequests, "Please wait before sending another request.")
 			return
 		}
 
-		_, err = database.Database.Exec(
-			"UPDATE rate_limits SET count = ?, last_request = ? WHERE ip = ?",
-			count+1, time.Now(), ip,
+		database.Database.Exec(
+			"UPDATE rate_limits SET last_request = ? WHERE ip = ?", time.Now(), ip,
 		)
-		if err != nil {
-			handlers.HandleError(w, http.StatusInternalServerError, "Internal Server Erro")
-			return
-		}
 
 		handler(w, r)
 	}
